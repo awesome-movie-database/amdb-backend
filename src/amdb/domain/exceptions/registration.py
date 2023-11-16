bdba132458f863a2256f07b9cf8b0c1d02694c4d
Egor <baseddepartmentzx77@gmail.com>
2023-11-16 21:20:35 +0600

    Add `RegistrationPolicy`

---
+++ src/amdb/domain/exceptions/registration.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+from .base import DomainException
+
+
+@dataclass(frozen=True, slots=True)
+class RegistrationDenied(DomainException):
+    
+    message: str
+
+    
