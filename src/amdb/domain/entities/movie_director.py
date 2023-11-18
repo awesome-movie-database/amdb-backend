0cce472f279578e57a688314a809fad80674c460
Egor <baseddepartmentzx77@gmail.com>
2023-11-18 21:19:08 +0600

    Add `MovieDirector` domain entity

---
+++ src/amdb/domain/entities/movie_director.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from datetime import datetime
+from uuid import UUID
+
+from .base import Entity
+
+
+@dataclass(slots=True)
+class MovieDirector(Entity):
+    
+    person_id: UUID
+    movie_id: UUID
+    created_at: datetime
+
+    @classmethod
+    def create(
+        cls, person_id: UUID, movie_id: UUID,
+        created_at: datetime
+    ) -> "MovieDirector":
+        return MovieDirector(
+            person_id=person_id, movie_id=movie_id,
+            created_at=created_at
+        )
