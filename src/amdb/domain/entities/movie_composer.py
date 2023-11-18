0ee66f8a79cd8d08edd2f57ccb565dcb9d7697e3
Egor <baseddepartmentzx77@gmail.com>
2023-11-18 21:22:13 +0600

    Add `MovieComposer` domain entity

---
+++ src/amdb/domain/entities/movie_composer.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from datetime import datetime
+from uuid import UUID
+
+from .base import Entity
+
+
+@dataclass(slots=True)
+class MovieComposer(Entity):
+
+    person_id: UUID
+    movie_id: UUID
+    created_at: datetime
+
+    @classmethod
+    def create(
+        cls, person_id: UUID, movie_id: UUID,
+        created_at: datetime
+    ) -> "MovieComposer":
+        return MovieComposer(
+            person_id=person_id, movie_id=movie_id,
+            created_at=created_at
+        )
