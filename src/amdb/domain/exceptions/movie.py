0950653b77cd1d9fc18319b27df4616bb32ef182
Egor <baseddepartmentzx77@gmail.com>
2023-11-22 03:22:57 +0600

    Remove `MovieNotUnderInspection` exception

--- src/amdb/domain/exceptions/movie.py
+++ src/amdb/domain/exceptions/movie.py
@@ -5,9 +5,3 @@ class MovieUnderInspection(DomainException):
     """
     Operation cannot be performed while movie is under inspection
     """
-
-
-class MovieNotUnderInspection(DomainException):
-    """
-    Operation cannot be performed while movie is not under inspection
-    """
