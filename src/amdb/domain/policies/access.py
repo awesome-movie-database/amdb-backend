de04376d79d6a6f4519a37f6e0e541dcd9262d9d
Egor <baseddepartmentzx77@gmail.com>
2023-11-25 02:04:09 +0600

    Add `AccessPolicy`

--- src/amdb/domain/policies/access.py
+++ src/amdb/domain/policies/access.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass
 from datetime import datetime
 from uuid import UUID
 
-from amdb.domain.exceptions.acess import AccessDenied
 from .base import Policy
 
 
@@ -12,15 +11,3 @@ class AccessPolicy(Policy):
     is_active: bool
     is_verified: bool
     created_at: datetime
-
-    def ensure_can_vote(self) -> None:
-        if not self.is_active:
-            raise AccessDenied()
-    
-    def ensure_can_review(self) -> None:
-        if not self.is_active:
-            raise AccessDenied()
-    
-    def ensure_can_rate_reviews(self) -> None:
-        if not self.is_active:
-            raise AccessDenied()
