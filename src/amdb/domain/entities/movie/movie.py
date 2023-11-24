a873dcd59526188215c26b6df86dfa81280a89cf
Egor <baseddepartmentzx77@gmail.com>
2023-11-25 03:46:28 +0600

    Add `UpdateMovie` service

--- src/amdb/domain/entities/movie/movie.py
+++ src/amdb/domain/entities/movie/movie.py
@@ -37,50 +37,6 @@ class Movie(Entity):
     kinopoisk_rating: Optional[float]
     kinopoisk_vote_count: Optional[int]
 
-    def update(
-        self,
-        title: Union[Title, Type[Unset]] = Unset,
-        runtime: Union[Runtime, None, Type[Unset]] = Unset,
-        release_date: Union[Date, None, Type[Unset]] = Unset,
-        genres: Union[list[Genre], None, Type[Unset]] = Unset,
-        countries: Union[list[str], None, Type[Unset]] = Unset,
-        production_status: Union[ProductionStatus, None, Type[Unset]] = Unset,
-        description: Union[str, None, Type[Unset]] = Unset,
-        summary: Union[str, None, Type[Unset]] = Unset,
-        budget: Union[Money, None, Type[Unset]] = Unset,
-        revenue: Union[Money, None, Type[Unset]] = Unset,
-        mpaa: Union[MPAA, None, Type[Unset]] = Unset,
-        filming_start: Union[Date, None, Type[Unset]] = Unset,
-        filming_end: Union[Date, None, Type[Unset]] = Unset,
-        imdb_id: Union[str, None, Type[Unset]] = Unset,
-        imdb_rating: Union[float, None, Type[Unset]] = Unset,
-        imdb_vote_count: Union[int, None, Type[Unset]] = Unset,
-        kinopoisk_id: Union[str, None, Type[Unset]] = Unset,
-        kinopoisk_rating: Union[float, None, Type[Unset]] = Unset,
-        kinopoisk_vote_count: Union[int, None, Type[Unset]] = Unset,
-    ) -> None:
-        self._update(
-            title=title,
-            runtime=runtime,
-            release_date=release_date,
-            genres=genres,
-            countries=countries,
-            production_status=production_status,
-            description=description,
-            summary=summary,
-            budget=budget,
-            revenue=revenue,
-            mpaa=mpaa,
-            filming_start=filming_start,
-            filming_end=filming_end,
-            imdb_id=imdb_id,
-            imdb_rating=imdb_rating,
-            imdb_vote_count=imdb_vote_count,
-            kinopoisk_id=kinopoisk_id,
-            kinopoisk_rating=kinopoisk_rating,
-            kinopoisk_vote_count=kinopoisk_vote_count,
-        )
-
     def add_amdb_votes(self, *votes: float) -> None:
         if self.is_under_inspection:
             raise movie_exceptions.MovieUnderInspection()
