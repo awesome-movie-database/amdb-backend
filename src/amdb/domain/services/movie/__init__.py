2d51845d9cca8686f4586a58b780c7dca4533aaa
Egor <baseddepartmentzx77@gmail.com>
2023-11-25 15:17:02 +0600

    Add `CreateSeries` service

---
+++ src/amdb/domain/services/movie/__init__.py
@@ -0,0 +1,5 @@
+from .create_movie import CreateMovie
+from .update_movie import UpdateMovie
+from .add_votes import AddVotesToMovie
+from .add_to_inspection import AddMovieToInspection
+from .remove_from_inspection import RemoveMovieFromInspection
