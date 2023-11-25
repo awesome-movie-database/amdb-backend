5d7b2d9bde148a4cc766771e97e812add9c7af73
Egor <baseddepartmentzx77@gmail.com>
2023-11-25 22:40:54 +0600

    Add `UpdateSeriesSeason` service

--- src/amdb/domain/entities/series/season/season.py
+++ src/amdb/domain/entities/series/season/season.py
@@ -27,62 +27,6 @@ class SeriesSeason(Entity):
     production_status: Optional[ProductionStatus]
     budget: Optional[Money]
 
-    @classmethod
-    def create(
-        cls,
-        series_id: UUID,
-        created_at: datetime,
-        episode_count: Optional[int] = None,
-        total_runtime: Optional[Runtime] = None,
-        release_date: Optional[Date] = None,
-        end_date: Optional[Date] = None,
-        is_ongoing: Optional[bool] = None,
-        genres: Optional[list[Genre]] = None,
-        countries: Optional[list[str]] = None,
-        production_status: Optional[ProductionStatus] = None,
-        budget: Optional[Money] = None,
-    ) -> "SeriesSeason":
-        return SeriesSeason(
-            series_id=series_id,
-            amdb_vote_count=0,
-            is_under_inspection=False,
-            created_at=created_at,
-            episode_count=episode_count,
-            amdb_rating=None,
-            total_runtime=total_runtime,
-            release_date=release_date,
-            end_date=end_date,
-            is_ongoing=is_ongoing,
-            genres=genres,
-            countries=countries,
-            production_status=production_status,
-            budget=budget,
-        )
-
-    def update(
-        self,
-        episode_count: Union[int, None, Type[Unset]] = Unset,
-        total_runtime: Union[Runtime, None, Type[Unset]] = Unset,
-        release_date: Union[Date, None, Type[Unset]] = Unset,
-        end_date: Union[Date, None, Type[Unset]] = Unset,
-        is_ongoing: Union[bool, None, Type[Unset]] = Unset,
-        genres: Union[list[Genre], None, Type[Unset]] = Unset,
-        countries: Union[list[str], None, Type[Unset]] = Unset,
-        production_status: Union[ProductionStatus, None, Type[Unset]] = Unset,
-        budget: Union[Money, None, Type[Unset]] = Unset,
-    ) -> None:
-        self._update(
-            episode_count=episode_count,
-            total_runtime=total_runtime,
-            release_date=release_date,
-            end_date=end_date,
-            is_ongoing=is_ongoing,
-            genres=genres,
-            countries=countries,
-            production_status=production_status,
-            budget=budget,
-        )
-
     def add_amdb_votes(self, *votes: float) -> None:
         if self.is_under_inspection:
             raise series_season_exceptions.SeriesSeasonUnderInspection()
