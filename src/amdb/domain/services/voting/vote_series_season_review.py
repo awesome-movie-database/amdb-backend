5d78131929a46f0222b3883bd6cb71e3ac2a4ab9
Egor <baseddepartmentzx77@gmail.com>
2023-12-07 13:07:26 +0600

    Add `VoteSeriesSeasonReview` service

---
+++ src/amdb/domain/services/voting/vote_series_season_review.py
@@ -0,0 +1,33 @@
+from amdb.domain.services.base import Service
+from amdb.domain.entities.review.series_season_review import SeriesSeasonReview
+from amdb.domain.entities.user.profile import Profile
+from amdb.domain.entities.vote.series_season_review_vote import SeriesSeasonReviewVote
+from amdb.domain.constants import VoteType
+from amdb.domain.exceptions.review import SeriesSeasonReviewNotApprovedError
+
+
+class VoteSeriesSeasonReview(Service):
+    def __call__(
+        self,
+        *,
+        season_review: SeriesSeasonReview,
+        reviewer_profile: Profile,
+        voter_profile: Profile,
+        type: VoteType,
+    ) -> SeriesSeasonReviewVote:
+        if not season_review.is_approved:
+            raise SeriesSeasonReviewNotApprovedError()
+
+        voter_profile.given_votes += 1
+        reviewer_profile.gained_votes += 1
+
+        if type is VoteType.LIKE:
+            season_review.likes += 1
+        elif type is VoteType.DISLIKE:
+            season_review.dislikes += 1
+
+        return SeriesSeasonReviewVote(
+            series_season_review_id=season_review.id,
+            user_id=voter_profile.user_id,
+            type=type,
+        )
