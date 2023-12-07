83985632e4c95f56526e9014fa0c94960074144b
Egor <baseddepartmentzx77@gmail.com>
2023-12-07 17:14:48 +0600

    Add `UpdateWatchlist` service

---
+++ src/amdb/domain/services/watchlist/update_watchlist.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from amdb.domain.services.base import Service
+from amdb.domain.entities.watchlist.watchlist import Watchlist
+
+
+class UpdateWatchlist(Service):
+    def __call__(
+        self,
+        *,
+        watchlist: Watchlist,
+        is_private: bool,
+        updated_at: datetime,
+    ) -> None:
+        watchlist.is_private = is_private
+        watchlist.updated_at = updated_at
