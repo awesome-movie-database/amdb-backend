c80100f66e374ffc1700323cd0fd15df71111eb8
Egor <baseddepartmentzx77@gmail.com>
2023-12-09 21:16:10 +0600

    Add `UpdateAchievement` service

---
+++ src/amdb/domain/services/achievement/update_achievement.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+from amdb.domain.services.base import Service
+from amdb.domain.entities.achievement.achievement import Achievement
+from amdb.domain.constants import Unset, unset
+
+
+class UpdateAchievement(Service):
+    def __call__(
+        self,
+        *,
+        achievement: Achievement,
+        title: Union[str, Unset] = unset,
+        description: Union[str, Unset] = unset,
+    ) -> None:
+        self._update_entity(
+            entity=achievement,
+            title=title,
+            description=description,
+        )
