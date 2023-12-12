aa46648ad169f3e99122a6d1dc0b744faffc84e1
Egor <baseddepartmentzx77@gmail.com>
2023-12-12 13:46:35 +0600

    Add `CheckAutoApprove` service

--- src/amdb/domain/entities/access_policy.py
+++ src/amdb/domain/entities/access_policy.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
+from typing import Optional
 
 from amdb.domain.entities.base import Entity
 from amdb.domain.entities.user.user import UserId
@@ -11,4 +12,5 @@ class AccessPolicy(Entity):
     is_active: bool
     is_verified: bool
     created_at: datetime
-    verified_at: datetime
+
+    verified_at: Optional[datetime]
