f5689f319765576f1cf1786b71133ea11ece75e6
Egor <baseddepartmentzx77@gmail.com>
2024-01-04 00:24:07 +0400

    Add alembic, Refactor `pyproject.toml`

---
+++ src/amdb/infrastructure/database/migrations/versions/9e92de201574_.py
@@ -0,0 +1,52 @@
+"""empty message
+
+Revision ID: 9e92de201574
+Revises:
+Create Date: 2024-01-03 19:31:09.011559
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '9e92de201574'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "movies",
+        sa.Column("id", sa.Uuid(), nullable=False),
+        sa.Column("title", sa.String(), nullable=False),
+        sa.Column("rating", sa.Float(), nullable=False),
+        sa.Column("rating_count", sa.Integer(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Uuid(), nullable=False),
+        sa.Column("name", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("name"),
+    )
+    op.create_table(
+        "ratings",
+        sa.Column("movie_id", sa.Uuid(), nullable=False),
+        sa.Column("user_id", sa.Uuid(), nullable=False),
+        sa.Column("value", sa.Float(), nullable=False),
+        sa.Column("created_at", sa.TIMESTAMP(timezone=True), nullable=True),
+        sa.ForeignKeyConstraint(["movie_id"], ["movies.id"], ondelete="CASCADE"),
+        sa.ForeignKeyConstraint(["user_id"], ["users.id"], ondelete="CASCADE"),
+        sa.PrimaryKeyConstraint("movie_id", "user_id"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("movies")
+    op.drop_table("users")
+    op.drop_table("ratings")
