28d3f4e37be36e5c937620a7ce13b632e879913e
Madnoberson <baseddepartmentzx77@gmail.com>
2024-02-23 13:49:44 +0400

    `UnitOfWork`: Remove rollback method

--- src/amdb/application/common/unit_of_work.py
+++ src/amdb/application/common/unit_of_work.py
@@ -4,6 +4,3 @@ from typing import Protocol
 class UnitOfWork(Protocol):
     def commit(self) -> None:
         raise NotImplementedError
-
-    def rollback(self) -> None:
-        raise NotImplementedError
