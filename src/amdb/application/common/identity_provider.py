1db6f89ee335facd42822680e9120c71820c3447
Madnoberson <baseddepartmentzx77@gmail.com>
2024-02-24 17:54:42 +0400

    Add doc strings to `IdentityProvider` protocol

--- src/amdb/application/common/identity_provider.py
+++ src/amdb/application/common/identity_provider.py
@@ -5,10 +5,22 @@ from amdb.domain.entities.user import UserId
 
 class IdentityProvider(Protocol):
     def user_id(self) -> UserId:
+        """
+        Returns current user id if authenticated,
+        otherwise raises error
+        """
         raise NotImplementedError
 
     def user_id_or_none(self) -> Optional[UserId]:
+        """
+        Returns current user id if authenticated,
+        otherwise returns None
+        """
         raise NotImplementedError
 
     def permissions(self) -> int:
+        """
+        Returns current user permissions if authenticated,
+        otherwise raises error
+        """
         raise NotImplementedError
