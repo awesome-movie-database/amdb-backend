8bd0bad7fd0480b4ac53c3448bb2af8f069cb316
Madnoberson <baseddepartmentzx77@gmail.com>
2024-03-06 15:50:31 +0400

    Refactor

--- src/amdb/presentation/create_handler.py
+++ src/amdb/presentation/create_handler.py
@@ -1,16 +1,14 @@
-__all__ = ("CreateHandler",)
-
 from typing import TypeVar, Protocol
 
 from amdb.application.common.identity_provider import IdentityProvider
 
 
-H = TypeVar("H", covariant=True)
+_H = TypeVar("_H", covariant=True)
 
 
-class CreateHandler(Protocol[H]):
+class CreateHandler(Protocol[_H]):
     def __call__(
         self,
         identity_provider: IdentityProvider,
-    ) -> H:
+    ) -> _H:
         raise NotImplementedError
